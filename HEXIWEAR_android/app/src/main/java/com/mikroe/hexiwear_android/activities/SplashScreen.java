30e53f22b602a3a577b07484429eb85d7af27ac0
Greg Frasco <frascog@wit.edu>
2016-08-17 13:22:51 -0400

    Added Main Activity with 3 Fragments Added Splash Screen with Logo

---
+++ HEXIWEAR_android/app/src/main/java/com/mikroe/hexiwear_android/activities/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.mikroe.hexiwear_android.activities;
+
+/**
+ * Created by frascog on 8/17/16.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
